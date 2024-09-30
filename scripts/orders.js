ed45cceaccb9defaab8e6586d70b48f296e69025
HugoB. <huguinhobastos@hotmail.com>
2024-09-30 23:03:36 +0100

    Start Tracking Page

--- scripts/orders.js
+++ scripts/orders.js
@@ -109,7 +109,7 @@ function orderDetails(order){
       </div>
 
       <div class="product-actions">
-        <a href="tracking.html">
+        <a href="tracking.html?orderId=${order.id}&productId=${matchingProduct.id}">
           <button class="track-package-button button-secondary">
             Track package
           </button>
